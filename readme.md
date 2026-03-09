7b1a9072cbd78f3f5da6407eeeff3e2fd7fd412c
Bithi <sumaiyaaktherbithi9@gmail.com>
2026-03-09 11:19:49 +0600

    first commit

---
+++ readme.md
@@ -0,0 +1,28 @@
+1️⃣  What is the difference between var, let, and const?
+
+Ans:
+var → function scoped and can be redeclared.
+let → block scoped and can be reassigned.
+const → block scoped and cannot be reassigned.
+
+2️⃣ What is the spread operator (...)?
+
+Ans:
+Used to expand elements of arrays or objects.
+
+3️⃣ What is the difference between map(), filter(), and forEach()?
+
+Ans:
+map() - transforms array.
+filter() - returns elements that match condition.
+forEach()	- loops through array.
+
+4️⃣ What is an arrow function?
+
+Ans:
+Short syntax for writing functions.
+
+5️⃣ What are template literals?
+
+Ans:
+Used for string interpolation using backticks.
---
+++ script.js
@@ -0,0 +1,155 @@
+const API = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+let issues = [];
+
+
+async function loadIssues(){
+
+document.getElementById("loader").classList.remove("hidden");
+
+let res = await fetch(API);
+let data = await res.json();
+
+issues = data.data;
+
+displayIssues(issues);
+
+document.getElementById("loader").classList.add("hidden");
+
+}
+
+
+function displayIssues(list){
+
+let container = document.getElementById("issueContainer");
+container.innerHTML = "";
+
+list.forEach(issue => {
+
+let borderColor =
+issue.status === "open"
+? "border-green-500"
+: "border-purple-500";
+
+let card = `
+<div onclick="openModal(${issue.id})"
+class="bg-white p-4 rounded-lg shadow border-t-4 ${borderColor} cursor-pointer">
+
+<h3 class="font-bold text-lg">${issue.title}</h3>
+
+<p class="text-sm text-gray-600 mb-2">
+${issue.description.slice(0,80)}...
+</p>
+
+<p>Status: ${issue.status}</p>
+<p>Category: ${issue.category}</p>
+<p>Author: ${issue.author}</p>
+<p>Priority: ${issue.priority}</p>
+<p>Label: ${issue.label}</p>
+<p class="text-xs text-gray-400">${issue.createdAt}</p>
+
+</div>
+`;
+
+container.innerHTML += card;
+
+});
+
+}
+
+
+let currentTab = "all";
+
+const tabActive = ['bg-[#4A00FF]', 'text-white', 'font-medium'];
+const tabInactive = ['bg-white', 'text-[#64748B]', 'border', 'border-[#E4E4E7]', 'font-medium'];
+
+function filterIssues(type){
+
+currentTab = type;
+
+// Filter issues
+if(type === "all"){
+displayIssues(issues);
+}
+
+if(type === "open"){
+let filtered = issues.filter(i => i.status === "open");
+displayIssues(filtered);
+}
+
+if(type === "closed"){
+let filtered = issues.filter(i => i.status === "closed");
+displayIssues(filtered);
+}
+
+// Change active tab
+switchTab(type);
+}
+function switchTab(tab){
+
+const tabs = ["all","open","closed"];
+
+for(const t of tabs){
+
+const tabName = document.getElementById("tab-" + t);
+
+if(t === tab){
+tabName.classList.remove(...tabInactive);
+tabName.classList.add(...tabActive);
+}else{
+tabName.classList.remove(...tabActive);
+tabName.classList.add(...tabInactive);
+}
+}
+}
+// Default load
+window.onload = function(){
+switchTab("all");
+filterIssues("all");
+}
+
+
+async function openModal(id){
+
+let res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+
+let data = await res.json();
+
+let issue = data.data;
+
+document.getElementById("modalTitle").innerText = issue.title;
+document.getElementById("modalDesc").innerText = issue.description;
+document.getElementById("modalAuthor").innerText = "Author: " + issue.author;
+document.getElementById("modalCategory").innerText = "Category: " + issue.category;
+document.getElementById("modalPriority").innerText = "Priority: " + issue.priority;
+document.getElementById("modalCreated").innerText = "Created: " + issue.createdAt;
+
+document.getElementById("modal").classList.remove("hidden");
+document.getElementById("modal").classList.add("flex");
+
+}
+
+
+function closeModal(){
+
+document.getElementById("modal").classList.add("hidden");
+
+}
+
+
+async function searchIssue(){
+
+let text = document.getElementById("searchInput").value;
+
+let res = await fetch(
+`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${text}`
+);
+
+let data = await res.json();
+
+displayIssues(data.data);
+
+}
+
+
+loadIssues();
